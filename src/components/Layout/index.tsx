a3717541afbc39aec9437e4f6c5baf86e9895178
Thach Do <ddthach@cmcglobal.vn>
2023-03-10 14:36:38 +0700

    update video

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -9,15 +9,37 @@ export type Props = {
 
 function Layout({ className, children }: Props) {
   return (
-    <div
-      className={cx([
-        className,
-        "layout mx-auto max-w-sm border rounded-3xl p-3 my-10",
-      ])}
-    >
-      <Header />
-      <Divider className="-mx-3" />
-      <div className="layout__body rounded-2xl">{children}</div>
+    <div className="grid grid-cols-5 gap-4">
+      <div
+        className={cx([
+          className,
+          "layout mx-auto max-w-sm w-full border rounded-3xl p-3 my-10",
+          "col-span-2",
+        ])}
+      >
+        <Header />
+        <Divider className="-mx-3" />
+        <div className="layout__body rounded-2xl">{children}</div>
+      </div>
+      <div className="mt-10 col-span-3">
+        <div className="mb-4 font-semibold">
+          The app's default value is the list includes random properties
+          (iconUrl, currentTime and 2FA code).
+        </div>
+        <ul className="ml-5 mx-auto list-disc">
+          <li className="mb-2">Max time for each looping is 60s.</li>
+          <li className="mb-2">
+            The code will be change and the currentTime will be reset to 60
+            affter the couter is the end
+          </li>
+          <li className="mb-2">The list is Sortable</li>
+          <li className="mb-2">
+            Can create a new item including (currentTime: 60; a new random code
+            and the name is the value is entered by user)
+          </li>
+          <li className="mb-2">Validate required field</li>
+        </ul>
+      </div>
     </div>
   );
 }
